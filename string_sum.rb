80bfc293c7d53f10bc3cef041a423c6550103121
Anamika <av.anamika1@gmail.com>
2024-12-24 01:27:36 +0530

    create string sum file

---
+++ string_sum.rb
@@ -0,0 +1,14 @@
+class StringSum
+  def self.add(data)
+    return 0 if data.empty?
+    delimiter = /,|\n/
+    if data.start_with?("//")
+        delimiter, data = data[2..].split("\n", 2)
+        delimiter = Regexp.escape(delimiter)
+    end
+    numbers = data.split(/#{delimiter}|,|\n/)
+    negatives = numbers.select { |n| n.to_i < 0 }
+    raise "negative values not allowed: #{negatives.join(', ')}" if negatives.any?
+    numbers.sum(&:to_i)
+  end
+end
